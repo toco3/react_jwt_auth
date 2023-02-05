c9ae59662500b2ae0668c9af5b26e29f0abe1ced
cehret <ce@toco3.com>
2023-02-05 18:28:09 +0100

    login

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,6 @@ const BASE_URL = 'https://t3-api2dev.vegan-masterclass.de/api';
 export default axios.create({
     baseURL: BASE_URL
 });
-
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type': 'application/json' },
